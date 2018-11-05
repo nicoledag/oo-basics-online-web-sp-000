74366b5e32673c0252f9cd27aacd62d7ad11534f
Nicole D <nnardella@gmail.com>
2018-11-05 17:03:07 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,16 +1,16 @@
 class Shoe
 
-attr_accessor :color, :size, :material, :condition
-attr_reader :brand
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
 
 
-def initialize(brand)
- @brand = brand
-end
+  def initialize(brand)
+   @brand = brand
+  end
 
-def cobble
-  puts "Your shoe is as good as new!"
-end
+  def cobble
+    puts "Your shoe is as good as new!"
+  end
 
 
 end
